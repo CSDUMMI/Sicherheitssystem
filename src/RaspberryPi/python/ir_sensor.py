fac0b9b787bd9c58b9cd74927a21a6927ba01b17
abbashan03 <42873886+abbashan03@users.noreply.github.com>
2018-12-25 22:47:04 +0100

    update delete

--- src/RaspberryPi/python/ir_sensor.py
+++ src/RaspberryPi/python/ir_sensor.py
@@ -1,37 +1,62 @@
+import os 
+from gpiozero import LED
 from picamera import PiCamera
 from time import sleep
 import RPi.GPIO as GPIO
 import time
 
 sensor = 18
+led = LED(17) 
 camera = PiCamera()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor,GPIO.IN)
 
-print  ("IR Sensor Ready.....")
+print  ("IR Sensor Ready.....") 
 
 nr = 0
 
-try:
+try: 
     while True:
-        if  GPIO.input(sensor):
+        if  GPIO.input(sensor): 
             print ("C")
             
 
-        else:
+      
+
+
+  else:
             print("O")
+            led.on()
             camera.start_preview()
             nr = nr + 1
             nrStr = str(nr)
-            fileName = '/media/pi/INTENSO/video' + nrStr + '.h264'
+            fileName = […]
+            […] '/media/pi/9016-4EF8/ […]
+            […]  video' + nrStr + '.h264' 
             camera.start_recording(fileName)
-            sleep(5)
+            sleep(600)
             camera.stop_recording()
             camera.stop_preview()
+            led.off()
 
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+
+
+    try:
+        while True:
+            if nr == 20:
+             for filename in os.listdir […]
+            […] ('/media/pi/9016-4EF8'): 
+                 os.remove('video' + nrStr + '.h264')
+                 print ("gelöscht")
+                 
+            else: print("ng")
+
+
+
+except KeyboardInterrupt: 
+    GPIO.cleanup()        
+
 
 
